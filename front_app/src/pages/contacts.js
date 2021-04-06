1e73eebc7637f94458006145cbc862b361276ce0
Alejandro Naranjo <ale.naranjo.1792@gmail.com>
2021-04-06 19:54:22 -0400

    feat(load data from backend)

--- front_app/src/pages/contacts.js
+++ front_app/src/pages/contacts.js
@@ -4,7 +4,7 @@ import { message, Card } from 'antd';
 import { Row, Col } from 'antd';
 import { Layout } from 'antd';
 import { Table, Tag } from 'antd';
-import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
+import { Get } from 'react-axios'
 
 import 'antd/dist/antd.css';
 import '../index.css';
@@ -14,7 +14,7 @@ const { Header, Content, Footer } = Layout;
 const columns = [
   {
     title: 'Id',
-    dataIndex: 'key',
+    dataIndex: 'id',
     key: 'id',
   },
   {
@@ -23,78 +23,24 @@ const columns = [
     key: 'name',
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'password',
+    dataIndex: 'password',
+    key: 'password',
   },
   {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
+    title: 'Phone',
+    dataIndex: 'phone',
+    key: 'phone',
   },
 ];
 
-
-const getUsersData() {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
-    console.log(res.data)
-    this.setState({loading:false, users: res.data})
-}
-
-componentDidMount(){
-    this.getUsersData()
-}
-
 const Contacts = () => {
-    axios.get('http://private-9ff5e-stackoverflow.apiary-mock.com/questions', {
-        responseType: 'json'
-    }).then(response => {
-        this.setState({ tableData: response.data });
-    });
-
-    /*
-    render() {
-  return (
-    <div>
-      <Get url="/api/user" params={{id: "12345"}}>
-        {(error, response, isLoading, makeRequest, axios) => {
-          if(error) {
-            return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
-          }
-          else if(isLoading) {
-            return (<div>Loading...</div>)
-          }
-          else if(response !== null) {
-            return (<div>{response.data.message} <button onClick={() => makeRequest({ params: { refresh: true } })}>Refresh</button></div>)
-          }
-          return (<div>Default message before request is made.</div>)
-        }}
-      </Get>
-    </div>
-  )
-}
-    */
-    return (
+    /*return (
         <Layout className="layout">
             <Content style={{ padding: '0 50px' }}>
                 <Row>
@@ -102,7 +48,33 @@ const Contacts = () => {
                 </Row>
             </Content>
         </Layout>
-    );
+    );*/
+    return (
+        <div>
+          <Get url="http://localhost:8080/contacts/list">
+            {(error, response, isLoading, makeRequest, axios) => {
+              if(error) {
+                return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
+              }
+              else if(isLoading) {
+                return (<div>Loading...</div>)
+              }
+              else if(response !== null) {
+                  console.log(response);
+                return (
+                    <Layout className="layout">
+                        <Content style={{ padding: '0 50px' }}>
+                            <Row>
+                                <Table dataSource={response.data} columns={columns} />
+                            </Row>
+                        </Content>
+                    </Layout>
+                )
+              }
+              return (<div>Default message before request is made.</div>)
+            }}
+          </Get>
+    </div>)
 };
 
 export default Contacts;
