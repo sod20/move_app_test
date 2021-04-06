f9ce352fdc9546616899f75bb1d9fe40a02ebc3f
Alejandro Naranjo <ale.naranjo.1792@gmail.com>
2021-04-06 15:02:45 -0400

    feat(login_page)

--- front_app/src/index.js
+++ front_app/src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { message, Card } from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { Row, Col } from 'antd';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const { Header, Content, Footer } = Layout;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const form_layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
+
+  const [date, setDate] = useState(null);
+  
+  const handleChange = value => {
+    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+    setDate(value);
+  };
+  
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+
+  return (
+    <Layout className="layout">
+      <Content style={{ padding: '0 50px' }}>
+        <Row>
+          <Col span={8}></Col>
+          <Col span={8}>
+          <div style={{ margin: '100px auto' }}>
+            <Card title="LOG IN">
+            <Form
+              {...form_layout}
+              name="basic"
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+            </Card>
+          </div>
+          </Col>
+          <Col span={8}> </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
+};
+
+render(<App />, document.getElementById('root'));
